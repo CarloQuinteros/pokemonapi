ce6315358f641e51975be860f886060b421f9c0a
CarloQuinteros <carloq24@gmail.com>
2025-12-10 15:45:13 -0400

    Feat: Buscador y filtro por tipo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchPokemon, setSearchPokemon] = useState("");
+  const [selectedType, setSelectedType] = useState([]);
+
+  const types = ["grass", "poison", "fire"];
+  console.log(selectedType);
 
   async function fetchData() {
     try {
@@ -30,21 +35,69 @@ function App() {
       setLoading(false);
     }
   }
+
+  function handleFilterPokemon(e) {
+    setSearchPokemon(e.target.value);
+  }
+
+  function handleTypeClick(type) {
+    if (selectedType.includes(type)) {
+      setSelectedType(selectedType.filter((t) => t !== type));
+    } else {
+      setSelectedType([...selectedType, type]);
+    }
+  }
+  function handleClearFilters() {
+    setSearchPokemon("");
+    setSelectedType([]);
+  }
+
   useEffect(() => {
     fetchData();
   }, []);
 
+  const filteredPokemons = pokemons.filter((pokemon) => {
+    return (
+      pokemon.name.toLowerCase().includes(searchPokemon.toLowerCase()) &&
+      (selectedType.length === 0 ||
+        pokemon.types.some((t) => selectedType.includes(t.type.name)))
+    );
+  });
+
+  console.log(filteredPokemons);
+
   return (
     <>
       <div className="container" style={{ margin: "80px" }}>
-        <div className="header"></div>
+        <div className="header">
+          <input
+            type="text"
+            placeholder="Search Pokemon by name..."
+            style={{ marginBottom: "30px", width: "500px", height: "30px" }}
+            value={searchPokemon}
+            onChange={handleFilterPokemon}
+          />
+          <button onClick={() => handleClearFilters()}>Clear filters</button>
+          {types.map((type) => {
+            return (
+              <button
+                key={type}
+                style={{ border: "1px solid black", borderRadius: "15%" }}
+                //onClick={() => setSelectedType(type)}
+                onClick={() => handleTypeClick(type)}
+              >
+                {type}
+              </button>
+            );
+          })}
+          <button onClick={() => setSelectedType([])}>All</button>
+        </div>
         <div
           className="main-container"
           style={{
             display: "flex",
-            justifyContent: "space-evenly",
+            justifyContent: "space-between",
             gap: "15px",
-
             flexWrap: "wrap",
             marginBottom: "10px",
           }}
@@ -52,7 +105,7 @@ function App() {
           {loading && <h1>Loading...</h1>}
           {error && <h1>{error}</h1>}
 
-          {pokemons.map((pokemon) => (
+          {filteredPokemons.map((pokemon) => (
             <div
               key={pokemon.id}
               style={{
